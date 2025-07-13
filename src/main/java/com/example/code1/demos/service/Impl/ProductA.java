0bfa2d9165a51d0850c6dc2995a2edd890f2d80f
wangsen <848766782@qq.com>
2025-07-13 16:39:41 +0800

    修改后第一次提交

--- src/main/java/com/example/code1/demos/service/Impl/ProductA.java
+++ src/main/java/com/example/code1/demos/service/Impl/ProductA.java
@@ -5,6 +5,8 @@ import com.example.code1.demos.service.Product;
 public class ProductA implements Product {
     @Override
     public void creat() {
+        System.out.println("wangsen!!");
+        System.out.println("come on!");
         System.out.println("ProductA is created.");
     }
 }
